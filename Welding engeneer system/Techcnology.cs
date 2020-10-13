9b4e054e14e6f3edcb6fbdbcd897b83cc548b37a
Александр Ивашев <toku_gawa@mail.ru>
2020-10-13 13:44:19 +0500

    Add project files.

---
+++ Welding engeneer system/Techcnology.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welding_engeneer_system
+{
+    class Techcnology
+    {
+    }
+}
---
+++ Welding engeneer system/WeldJoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welding_engeneer_system
+{
+    class WeldJoint
+    {
+        Material material = new Material();
+        Techcnology technology = new Techcnology();
+        Specialist specialist = new Specialist();
+        Equipment equipment = new Equipment();
+        Qualification qualification = new Qualification();
+        Elements element1 = new Elements();
+        Elements element2 = new Elements();
+
+    }
+}
